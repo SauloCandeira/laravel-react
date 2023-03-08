81ff32b786c65e344b1bd222338901e541339401
Saulo Candeira <saulocandeira@hotmail.com>
2023-03-08 16:30:28 -0300

    08/03/2023

--- front-end/src/components/Tables/Table4/Table4.tsx
+++ front-end/src/components/Tables/Table4/Table4.tsx
@@ -27,6 +27,7 @@ export default function Table4({ data ,nome, hover = true, striped = true, varia
     // console.log('data', data)
     // const [showModal, setShowModal] = useState(false)
     const [showModal, setShowModal] = useState({ isOpen: false, postId: null });
+    const [filteredList, setFilteredList] = useState(data);
 
     // Fechar modal
     const handleOnClose = () => setShowModal({ isOpen: false, postId: null})
@@ -58,6 +59,33 @@ export default function Table4({ data ,nome, hover = true, striped = true, varia
 
 
 
+
+    const filterBySearch = () => {
+        // Access input value
+        // const query = event.target.value;
+        // Create copy of item list
+        // var updatedList = [...data];
+        // Include all elements which includes the search query
+        var updatedList = data?.filter((item) => {
+            return query.toLowerCase() === '' 
+            ? item : (format(new Date(item.dt_inicio), 'ddMMyyyy')+-item.id_os).includes(query) 
+            || item.condominio.no_condominio.toLowerCase().includes(query)
+        });
+        // Trigger render with updated values
+
+        
+        setFilteredList(updatedList);
+        console.log('filteredList', filteredList?.length)
+        // console.log('filteredList',filteredList)
+        // console.log('updatedList',updatedList)
+    };
+
+
+
+
+    useEffect(() => {
+        filterBySearch()
+    }, [query]);
     // const teste = data.filter((item) => item.startsWith(query))
     // console.log('teste', teste)
 
@@ -65,20 +93,22 @@ export default function Table4({ data ,nome, hover = true, striped = true, varia
     //     arquivar();
     // }, []);
     
-    console.log('query', query)
-    console.log('data', data)
+    // console.log('query', query)
+    // console.log('data', data)
     return (
-		<div className="bg-white px-4 pt-3 pb-4 rounded-sm flex-1">
-            <strong className="text-gray-700 font-medium">{nome}</strong>
+        <>
 
-            {data && data.filter((item) => {
-                return query.toLowerCase() === '' 
-                ? item : (format(new Date(item.dt_inicio), 'ddMMyyyy')+-item.id_os).includes(query) 
-                || item.condominio.no_condominio.toLowerCase().includes(query)
-            }).map((Client, index) => {  
-                return ( 
+            <div className="bg-white px-4 pt-3 pb-4 rounded-sm flex-1">
+
+                <strong className="text-gray-700 font-medium">{nome}</strong>
+
+                {filteredList?.length <= 0 && 
+                    <h2 style={{ textAlign: 'center', fontStyle: 'italic' }}> Nenhuma OS </h2>
+                }
+
+                {filteredList?.length > 0 &&
                     <table key={Math.random()} className="table-auto overflow-x-auto w-full block">
-                        {/* {data &&  */}
+    
                         <thead>
                             <tr style={{fontSize: '10px', color: '#8898aa'}}>
                                 <th> CLIENTE </th>
@@ -91,98 +121,106 @@ export default function Table4({ data ,nome, hover = true, striped = true, varia
                                 <th></th>
                             </tr>
                         </thead>
-                        {/* } */}
-                        <tbody>
-                            
-                            <tr key={index} className={`${hover && "hover"}`} >
-                                <td className="pl-8"> 
-                                    {Client.condominio.no_condominio} 
-                                </td>
-                                <td style={{ alignItems: 'center', textAlign: 'center'}}> 
-                                    {Client.nu_prioridade == 3 && (<> <Label variant="default"> Alta </Label> </>)} 
-                                    {Client.nu_prioridade == 2 && (<> <Label variant="secondary"> Média </Label> </>)}
-                                    {Client.nu_prioridade == 1 && (<> <Label variant="primary"> Baixa </Label> </>)}                                    
-                                </td>
-                                <td>
 
-                                    {/* <div className="group flex relative">
-                                        <span className="bg-red-400 text-white px-2 py-1">Button</span>
-                                        <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
-                                        -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Tooltip</span>
-                                    </div> */}
-
-                                    
-                                    <Tooltip message={"Clique para copiar"}>
-                                        <Button 
-                                            size="m" 
-                                            variant="tertiary"
-                                            onClick={() =>  navigator.clipboard.writeText(format(new Date(Client.dt_inicio), 'ddMMyyyy')+-Client.id_os)}
-                                        > 
-                                            {format(new Date(Client.dt_inicio), 'ddMMyyyy')+-Client.id_os}
-                                        </Button> 
-                                    </Tooltip>
-                                    
-
-                                </td>
-                                <td> {format(new Date(Client.dt_inicio), 'dd/MM/yyyy kk:mm:ss')} </td>
-                                <td> <Badge variant="primary"/>  {Client.status.no_os_status} </td>
-                                <td> <Label variant='tertiary'>{Client.tipo.no_os_tipo} </Label> </td>
-                                <td> 
-                                <div key={Math.random()} style={{ display: 'grid',  margin: '0px 0px 0px 0px', gridTemplateColumns: 'repeat(auto-fill, 20px)'}}>
-                                    {Client.funcionario?.map((Funcionario, index) => {
-                                        return (
-                                            <div key={index}>
-                                                <Tooltip message={Funcionario.no_funcionario}>
-                                                    <Avatar> 
-                                                        {Funcionario.no_funcionario.charAt(0)} 
-                                                    </Avatar> 
-                                                </Tooltip>
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                                </td>
-                                <td>
-
-                                    <div className="flex flex-row">
-                                        <Button 
-                                            onClick={() =>  setShowModal({ isOpen: true, postId: Client.id_os  })}
-                                            size="p" 
-                                            // variant="quaternary"
-                                                variant="outline"
-                                        > 
-
-                                            <div className="text-center inline-flex items-center mr-2 ">
-                                                <FaEye className="m-1 h-3" /> Ver
-                                            </div>
-
-                                        </Button>
-
-                                        <ModalY onClose={handleOnClose} id={showModal.postId} visible={showModal.isOpen}/>
-
-                                    
-                                        <Button 
-                                            size="m" 
-                                            // variant="tertiary"
-                                            onClick={() => arquivar(Client.id_os)}
-                                            variant="secondary"
-                                        > 
-                                            <div className="text-center inline-flex items-center mr-2 ">
-                                                <FaArchive className="m-1 h-3"/> Arquivar
-                                            </div>
-                                        </Button>
-        
-                                    </div>
-                                
-                                </td>
-                            </tr>
                         
-                        </tbody>
-                    </table>
+                        <tbody>
 
-                )
-            })}
-            {data ? null : <h2 style={{ textAlign: 'center', fontStyle: 'italic' }}>Nenhuma OS</h2>}
-		</div>
+                            {filteredList?.map((Client, index) => {
+
+                                return (
+                                    <tr key={index} className={`${hover && "hover"}`} >
+                                        <td className="pl-8"> 
+                                            {Client.condominio.no_condominio} 
+                                        </td>
+                                        <td style={{ alignItems: 'center', textAlign: 'center'}}> 
+                                            {Client.nu_prioridade == 3 && (<> <Label variant="default"> Alta </Label> </>)} 
+                                            {Client.nu_prioridade == 2 && (<> <Label variant="secondary"> Média </Label> </>)}
+                                            {Client.nu_prioridade == 1 && (<> <Label variant="primary"> Baixa </Label> </>)}                                    
+                                        </td>
+                                        <td>
+
+                                            {/* <div className="group flex relative">
+                                                <span className="bg-red-400 text-white px-2 py-1">Button</span>
+                                                <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
+                                                -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Tooltip</span>
+                                            </div> */}
+
+                                            
+                                            <Tooltip message={"Clique para copiar"}>
+                                                <Button 
+                                                    size="m" 
+                                                    variant="tertiary"
+                                                    onClick={() =>  navigator.clipboard.writeText(format(new Date(Client.dt_inicio), 'ddMMyyyy')+-Client.id_os)}
+                                                > 
+                                                    {format(new Date(Client.dt_inicio), 'ddMMyyyy')+-Client.id_os}
+                                                </Button> 
+                                            </Tooltip>
+                                            
+
+                                        </td>
+                                        <td> {format(new Date(Client.dt_inicio), 'dd/MM/yyyy kk:mm:ss')} </td>
+                                        <td> <Badge variant="primary"/>  {Client.status.no_os_status} </td>
+                                        <td> <Label variant='tertiary'>{Client.tipo.no_os_tipo} </Label> </td>
+                                        <td> 
+                                        <div key={Math.random()} style={{ display: 'grid',  margin: '0px 0px 0px 0px', gridTemplateColumns: 'repeat(auto-fill, 20px)'}}>
+                                            {Client.funcionario?.map((Funcionario, index) => {
+                                                return (
+                                                    <div key={index}>
+                                                        <Tooltip message={Funcionario.no_funcionario}>
+                                                            <Avatar> 
+                                                                {Funcionario.no_funcionario.charAt(0)} 
+                                                            </Avatar> 
+                                                        </Tooltip>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                        </td>
+                                        <td>
+
+                                            <div className="flex flex-row">
+                                                <Button 
+                                                    onClick={() =>  setShowModal({ isOpen: true, postId: Client.id_os  })}
+                                                    size="p" 
+                                                    // variant="quaternary"
+                                                        variant="outline"
+                                                > 
+
+                                                    <div className="text-center inline-flex items-center mr-2 ">
+                                                        <FaEye className="m-1 h-3" /> Ver
+                                                    </div>
+
+                                                </Button>
+
+                                                <ModalY onClose={handleOnClose} id={showModal.postId} visible={showModal.isOpen}/>
+
+                                            
+                                                <Button 
+                                                    size="m" 
+                                                    // variant="tertiary"
+                                                    onClick={() => arquivar(Client.id_os)}
+                                                    variant="secondary"
+                                                > 
+                                                    <div className="text-center inline-flex items-center mr-2 ">
+                                                        <FaArchive className="m-1 h-3"/> Arquivar
+                                                    </div>
+                                                </Button>
+                
+                                            </div>
+                                        
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+
+                    </table>
+                            
+                }
+                
+                {data ? null : <h2 style={{ textAlign: 'center', fontStyle: 'italic' }}>Nenhuma OS</h2>}
+            
+            </div>
+        </>
 	)
 }
--- front-end/src/pages/Dashboard/DashboardY.tsx
+++ front-end/src/pages/Dashboard/DashboardY.tsx
@@ -21,6 +21,7 @@ export default function DashboardY() {
   const [empresas, setEmpresas] = useState([]);
   const [tipos, setTipos] = useState([]);
   const [osStatus, setOsStatus] = useState([]);
+  const [query, setQuery] = useState("");
 
   const handleOnClose = () => setShowModal(false)
 
@@ -115,7 +116,14 @@ export default function DashboardY() {
 
           <div className="flex flex-col flex-1 py-8 p-4">
             <div className="relative z-0 w-full group">
-              <input type="text" id="cliente" className="block py-2.5 px-0 w-full text-sm text-gray-400  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+              <input 
+                type="text" 
+                id="cliente" 
+                className="block py-2.5 px-0 w-full text-sm text-gray-400  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                placeholder=" " 
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              
               <label htmlFor="floating_cliente" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Digite algo para filtrar </label>
             </div>
           </div>
@@ -130,7 +138,7 @@ export default function DashboardY() {
                 
                 <div key={index} style={{marginTop: '20px', backgroundColor: '#fff'}}>
                   {item.lista_de_os.length === 0 && (<> <Table4 data={null} nome={item.no_os_status} columns={null} hover={true} striped={true} /> </>)}
-                  {item.lista_de_os.length > 0 && (<> <Table4 data={item.lista_de_os} nome={item.no_os_status} columns={null} hover={true} striped={true} /> </>)}
+                  {item.lista_de_os.length > 0 && (<> <Table4 data={item.lista_de_os} nome={item.no_os_status} columns={null} hover={true} striped={true} query={query} /> </>)}
                 </div>
               )
 
